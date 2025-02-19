b7a2355efa84f43716967d6b720f4a401be35a1e
boshraemad <boshraemad2004@icloud.com>
2025-02-20 00:00:40 +0200

    Auth-version-2

--- src/components/verifyEmail.jsx
+++ src/components/verifyEmail.jsx
@@ -7,31 +7,31 @@ const VerifyEmail = () => {
   const [status, setStatus] = useState('Verifying');
   const [searchParams] = useSearchParams();
   const navigate=useNavigate();
-  // useEffect(() => {
-  //   const verifyEmail = async () => {
-  //     const token = searchParams.get('token'); // Get token from URL
-  //     if (!token) {
-  //       setStatus('Invalid verification link.');
-  //       return;
-  //     }
+  useEffect(() => {
+    const verifyEmail = async () => {
+      const token = searchParams.get('token'); // Get token from URL
+      if (!token) {
+        setStatus('Invalid verification link.');
+        return;
+      }
 
-  //     try {
-  //       const response = await axios.get(`'https://learning-management-system-262s.onrender.com/api/v1/verify-Email`, { token });
-  //       setStatus("Email verified, now you can login to your account.");
-  //     } catch (error) {
-  //       setStatus(error.response.data.message || 'Verification failed. Try again.');
-  //     }
-  //   };
+      try {
+        const response = await axios.get(`'https://learning-management-system-262s.onrender.com/api/v1/verify-Email`, { token });
+        setStatus("Email verified, now you can login to your account.");
+      } catch (error) {
+        setStatus(error.response.data.message || 'Verification failed. Try again.');
+      }
+    };
 
-  //   verifyEmail();
-  // }, [searchParams]);
+    verifyEmail();
+  }, [searchParams]);
 
   return (
     <div>
-      {/* <h2>{status}</h2>
+      <h2>{status}</h2>
       {status === 'Email verified successfully! You can now log in.' && (
         navigate("/login")
-      )} */}
+      )}
     </div>
   );
 };
